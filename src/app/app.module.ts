11fb582fa389c27ff523ee973f24c2fffcd83d71
EnriquePSouza <enriquepdsz@gmail.com>
2021-03-19 12:23:53 -0300

    Created - Angular aplication components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
+// Imports do angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// imports de configs.
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// imports dos components.
+import { AlunosComponent } from './components/alunos/alunos.component';
+import { ProfessoresComponent } from './components/professores/professores.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { TituloComponent } from './components/shared/titulo/titulo.component';
+
+// imports de modulos que não são angular.
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent,
+    ProfessoresComponent,
+    SobreComponent,
+    DashboardComponent,
+    NavComponent,
+    TituloComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    NgxSpinnerModule,
+    ToastrModule.forRoot({ // Configs padrões para usar sempre com toastr.
+      timeOut: 3500,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+      closeButton: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
